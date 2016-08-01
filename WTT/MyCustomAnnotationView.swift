e8a85ba2db0c62856be18499febadbbd6dd3a99b
Lin Wei <linwei365@gmail.com>
2016-08-01 15:46:45 -0700

    added custome annotationView

---
+++ WTT/MyCustomAnnotationView.swift
@@ -0,0 +1,21 @@
+//
+//  MyCustomAnnotationView.swift
+//  WTT
+//
+//  Created by Lin Wei on 8/1/16.
+//  Copyright © 2016 Lin Wei. All rights reserved.
+//
+
+import UIKit
+
+class MyCustomAnnotationView: MKAnnotationView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
