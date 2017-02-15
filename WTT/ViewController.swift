19a0c967e8657d8e45229344bca436f02c352c3e
Lin Wei <linwei365@gmail.com>
2017-02-15 12:03:21 -0800

    Initial Commit

---
+++ WTT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WTT
+//
+//  Created by Lin Wei on 2/15/17.
+//  Copyright © 2017 Lin Wei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
